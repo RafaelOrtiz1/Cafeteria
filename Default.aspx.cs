d35fac202444370e0e650a632aaa712a86e83cb8
Jose Rafael Garcia Ceme <a20300102@unideh.edu.mx>
2024-11-05 13:28:52 -0600

    Agregar archivos de proyecto.

---
+++ Default.aspx.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml;
+using System.Xml.Xsl;
+
+public partial class _Default : System.Web.UI.Page
+{
+    //Declarar una variable global
+    public string tipomenu = string.Empty;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        //Recuperar variable validando si existe en URL (xml)
+
+        if (Request.QueryString["id"] == null) {
+            tipomenu = "0";
+        }else {
+            tipomenu = Request.QueryString["id"];
+        }
+
+        TranformarXML();
+    }
+
+
+    //Procesador de XSLT
+    private void TranformarXML() {
+        //Recuperamos rutas de XML Y XSL
+
+        string xmlPath = ConfigurationManager.AppSettings["FileServer"].ToString() + "xml/menu.xml";
+        string xsltPath = ConfigurationManager.AppSettings["FileServer"].ToString() + "xsl/Template.xslt";
+
+        //Necesitamos inyeccion de dependencia colocar arriba en dependencias "using Sistem.xml"
+        XmlTextReader xmlTextReader = new XmlTextReader(xmlPath);
+
+        //Configuramos las credenciales para resolver recursos externos como XSLT
+
+        XmlUrlResolver resolver = new XmlUrlResolver();
+        resolver.Credentials = CredentialCache.DefaultCredentials;
+
+        //Credenciales para poder acceder XSLT
+        //true son para ser tratados como un documento
+
+        XsltSettings settings = new XsltSettings(true, true);
+
+        //Leemos el archivo slt y lo cargamos para su transformacion
+
+        XslCompiledTransform xslt = new XslCompiledTransform();
+        xslt.Load(xsltPath, settings, resolver);
+
+        //Almacenamos el resultado de la informacion
+
+        StringBuilder stringBuilder = new StringBuilder();
+        TextWriter textwriter = new StringWriter(stringBuilder);
+
+        //Configuramos los argumentos para la trasnformacion
+
+        XsltArgumentList xsltArgumentList = new XsltArgumentList();
+
+        //Pasamos la variable de tipo menu a xslt
+        xsltArgumentList.AddParam("TipoMenu", "", tipomenu);
+
+        //Trasnformamos el XML A HTML usando XSLT
+
+        xslt.Transform(xmlTextReader, xsltArgumentList, textwriter );
+        
+        //Obtenemos el resultado de la transformacion en una sola cadena
+        
+        string resultado = stringBuilder.ToString();
+
+        //escribimos el resultado como una respuesta HTTP
+        Response.Write( resultado );
+
+    }
+
+}
---
+++ xsl/Template.xslt
@@ -0,0 +1,22 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
+    xmlns:msxsl="urn:schemas-microsoft-com:xslt" exclude-result-prefixes="msxsl"
+>
+	
+	<!--La etiqueta output para determinar el metodo de salida-->
+    <xsl:output method="html" indent="yes"/>
+	
+	<!--creamos variable que reciba parametro desde el procesador-->
+	<!--name el nombre de la variable-->
+	<!--selec establece el origen-->
+
+	<xsl:param name = "TipoMenu" select="TipoMenu"></xsl:param>
+	
+	<!--El primer template que haga match con el nood padre de XML-->
+
+	<xsl:template match="Menu">
+		<xsl:value-of select="Datos/NombreRestaurant"/>
+	</xsl:template>
+	
+	
+</xsl:stylesheet>
